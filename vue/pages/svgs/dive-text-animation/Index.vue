4bd3463c4bf4b779a84457ad4f8debe5c4f6cd54
accup <32096121+accup@users.noreply.github.com>
2022-08-20 23:24:19 +0900

    wip: simplify svg

--- vue/pages/svgs/dive-text-animation/Index.vue
+++ vue/pages/svgs/dive-text-animation/Index.vue
@@ -1,33 +1,26 @@
+<script setup lang="ts"></script>
+
 <template>
-  <svg id="book" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <svg id="book" viewBox="0 0 640 720" xmlns="http://www.w3.org/2000/svg">
     <defs>
-      <path id="book__spade">
-        <animate
-          attributeName="d"
-          values="
-              M50 10Q35 27 20 40C-10 66 30 100 50 70L40 90L60 90L50 70C70 100 110 66 80 40Q65 27 50 10Z;
-              M5 15L45 15M5 25L45 25M5 35L45 35M5 45L45 45M5 55L45 55M5 65L45 65Z
-            "
-          dur="10s"
-          repeatCount="indefinite"
-        />
-      </path>
-    </defs>
-    <use
-      stroke="black"
-      stroke-width="1"
-      stroke-dasharray="50 50"
-      fill="none"
-      href="#book__spade"
-    />
-    <text fill="white" font-family="PlemolJP">
-      <animate
-        attributeName="font-size"
-        values="1;7;7"
-        dur="10s"
-        repeatCount="indefinite"
+      <path
+        id="book__spade"
+        d="
+          M50 10
+          Q35 27 20 40
+          C-10 66 30 100 50 70
+          L40 90
+          L60 90
+          L50 70
+          C70 100 110 66 80 40
+          Q65 27 50 10
+          Z
+        "
       />
-      <textPath href="#book__spade" textLength="300">
+    </defs>
+    <use stroke="white" stroke-width="1" fill="none" href="#book__spade" />
+    <text fill="black" dy="1" font-family="PlemolJP" font-size="3">
+      <textPath href="#book__spade" textLength="310">
         あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。
       </textPath>
     </text>
@@ -36,6 +29,6 @@
 
 <style scoped>
 #book {
-  background-color: gray;
+  background-color: lightgray;
 }
 </style>
