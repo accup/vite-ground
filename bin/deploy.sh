e863b747994949d7f72d52a56d97e7433cb64644
accup <32096121+accup@users.noreply.github.com>
2022-08-21 23:35:07 +0900

    chore: add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+# from https://ja.vitejs.dev/guide/static-deploy.html and modified
+
+# エラー時は停止
+set -e
+
+# スクリプトの場所から開始
+cd `dirname $0`
+
+# プロジェクトルートに移動
+cd ../
+
+# ビルド
+npm run build
+
+# ビルド出力ディレクトリに移動
+cd dist
+
+# カスタムドメインにデプロイする場合
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# https://<USERNAME>.github.io にデプロイする場合
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# https://<USERNAME>.github.io/<REPO> にデプロイする場合
+git push -f git@github.com:accup/vite-ground.git main:gh-pages
+
+cd -
