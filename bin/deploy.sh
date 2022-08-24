84cff96963422c81c1d7cc4694bd9de5977bf2dd
accup <32096121+accup@users.noreply.github.com>
2022-08-24 18:05:07 +0900

    fix: hide reset button and meet

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -29,6 +29,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # https://<USERNAME>.github.io/<REPO> にデプロイする場合
-git push -f git@github.com:accup/vite-ground.git main:gh-pages
+git push -f git@github-accup:accup/vite-ground.git main:gh-pages
 
 cd -
